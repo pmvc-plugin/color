477cef30800dc3b241c194fc755303c96e2fe6ad
Hill <hill@kimo.com>
2015-08-01 01:01:15 +0400

    update

--- src/BaseColor.php
+++ src/BaseColor.php
@@ -14,6 +14,28 @@ class BaseColor
        $this->b = $b;
     }
 
+    function getClone()
+    {
+        return clone($this);
+    }
+
+    function decrease(BaseColor $other)
+    {
+        $this->r -= $other->r;
+        $this->g -= $other->g;
+        $this->b -= $other->b;
+        return $this;
+    }
+
+    function setAlpha(BaseColor $other, $alpha)
+    {
+        $this->decrease($other);
+        $other = $other->toArray();
+        $this->r = $this->r * $alpha + $other['r'];
+        $this->g = $this->g * $alpha + $other['g'];
+        $this->b = $this->b * $alpha + $other['b'];
+    }
+
     function toHex()
     {
         $colorstr='';
@@ -23,4 +45,26 @@ class BaseColor
         return $colorstr;
     }
 
+    function toArray()
+    {
+        return array(
+            'r'=>$this->r
+            ,'g'=>$this->g
+            ,'b'=>$this->b
+        );
+    }
+
+    /**
+     * @param resource $image gd image resource http://php.net/manual/en/function.imagecreatetruecolor.php
+     * @see http://php.net/manual/en/function.imagecolorallocate.php
+     */
+    function toGd($image)
+    {
+        return imagecolorallocate(
+            $image,
+            $this->r,
+            $this->g,
+            $this->b
+        ); 
+    }
 }
